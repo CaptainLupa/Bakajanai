f8fc20002b5b77a643d6ebb32e2e35440af2775b
Jens Eckert <capt.lupa@gmail.com>
2021-10-30 15:56:31 -0400

    This too

---
+++ Bakajanai/headers/precomp.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#define _WIN32_WINNT 0x0500
+//#define STB_IMAGE_IMPLEMENTATION
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include "stb_image.h"
+//#include "stbpain.c"
+
+#include <Windows.h>
+#include <iostream>
+#include <conio.h>
+#include <string>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <initializer_list>
+#include <algorithm>
+#include "GhostTitle.h"
